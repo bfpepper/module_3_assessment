b33c675d181ea0075189f5ab5a2edf9609fd43c2
Ben Pepper <bfpepper87@gmail.com>
2017-01-06 10:22:05 -0700

    add connection to service.

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -1,10 +1,12 @@
 class StoreService
 
   def initialize
-    @connection = Faraday.new("https://api.bestbuy.com/v1/stores?format=json")
+    @connection = Faraday.new("https://api.bestbuy.com/v1/stores")
   end
+  # https://api.bestbuy.com/v1/stores
 
   def get_stores(zipcode)
+    stores = @connection.get "(area(#{zipcode},25))?format=json&show=storeType,city,distance,phone,longName&apiKey=#{ENV['api_key']}"
     binding.pry
   end
 
